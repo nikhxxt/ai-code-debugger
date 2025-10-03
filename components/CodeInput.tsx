545a90ca2ab971a192073191f1495f7c0be355c0
nikhxxt <nikhxtwork@gmail.com>
2025-10-03 22:39:59 +0530

    Update CodeInput.tsx

--- components/CodeInput.tsx
+++ components/CodeInput.tsx
@@ -9,9 +9,10 @@ export default function CodeInput({
     <textarea
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      placeholder="Paste your Python code here..."
+      placeholder="Paste your code here..."
       className="w-full h-48 p-4 bg-gray-900 text-white placeholder-gray-400 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 font-mono resize-none"
     />
   );
 }
+
 
