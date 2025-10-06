d34e06c7d8e823a3721d8db3417cd2bdb2408822
nikhxxt <nikhxtwork@gmail.com>
2025-10-06 15:20:12 +0530

    Update LanguageSelector.tsx

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -18,6 +18,7 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = ({ language, setLangua
         onChange={(e) => setLanguage(e.target.value)}
         className="w-full p-2 border border-blue-400 rounded bg-white text-black"
       >
+        <option value="auto">Auto Detect (Universal)</option>
         <option value="python">Python</option>
         <option value="javascript">JavaScript</option>
         <option value="typescript">TypeScript</option>
@@ -28,6 +29,10 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = ({ language, setLangua
         <option value="go">Go</option>
         <option value="ruby">Ruby</option>
         <option value="rust">Rust</option>
+        <option value="css">CSS</option>
+        <option value="html">HTML</option>
+        <option value="bash">Bash</option>
+        <option value="json">JSON</option>
       </select>
     </div>
   );
