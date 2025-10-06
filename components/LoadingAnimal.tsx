205d70105f2965121d8448e6c4d99e651e8891ca
nikhxxt <nikhxtwork@gmail.com>
2025-10-06 15:26:56 +0530

    Update LoadingAnimal.tsx

--- components/LoadingAnimal.tsx
+++ components/LoadingAnimal.tsx
@@ -3,28 +3,16 @@
 import { useEffect, useState } from 'react';
 
 const gifs = [
-  'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif', // cat
-  'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',   // dog
-  'https://media.giphy.com/media/13borq7Zo2kulO/giphy.gif',      // panda
-  'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif', // bunny
-  'https://media.giphy.com/media/3oEduSbSGpGaRX2Vri/giphy.gif',  // hamster
-  'https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif',  // duck
-  'https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif',  // baby sloth
-  'https://media.giphy.com/media/3o6ZsY8jvYyU8Z1xkU/giphy.gif',  // piglet
-  'https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif',  // koala
-  'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif'    // puppy repeat for variety
+  'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif', // Cat
+  'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',   // Dog
+  'https://media.giphy.com/media/13borq7Zo2kulO/giphy.gif',      // Panda
+  'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif'  // Bunny
 ];
 
-function shuffle(array: string[]) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
 export default function LoadingAnimal() {
-  const [gifList, setGifList] = useState<string[]>([]);
   const [gifIndex, setGifIndex] = useState(0);
 
   useEffect(() => {
-    setGifList(shuffle(gifs)); // shuffle on mount
     const interval = setInterval(() => {
       setGifIndex((prev) => (prev + 1) % gifs.length);
     }, 3000); // rotate every 3 seconds
@@ -34,7 +22,7 @@ export default function LoadingAnimal() {
   return (
     <div className="flex flex-col items-center justify-center mt-6">
       <img
-        src={gifList[gifIndex]}
+        src={gifs[gifIndex]}
         alt="Cute animal loading"
         className="w-32 h-32 rounded-full shadow-lg"
       />
