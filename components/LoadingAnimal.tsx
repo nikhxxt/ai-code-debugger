2c15d6f21f1ccfffe2674ffabba373543d92cda9
nikhxxt <nikhxtwork@gmail.com>
2025-10-06 15:48:59 +0530

    Update LoadingAnimal.tsx

--- components/LoadingAnimal.tsx
+++ components/LoadingAnimal.tsx
@@ -3,19 +3,22 @@
 import { useEffect, useState } from 'react';
 
 const gifs = [
-  'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif', // Cat typing
-  'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',   // Dog wagging tail
-  'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif', // Bunny hopping
-  'https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif'   // Koala waving
+  'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif',       // Cat typing
+  'https://media3.giphy.com/media/efZeeUTH7aRdxZVjJW/giphy.gif',      // Dog head tilt
+  'https://media.giphy.com/media/5xaOcLGvzHxDKjufnLW/giphy.gif',      // Bunny hopping
+  'https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif',       // Koala waving
+  'https://media.giphy.com/media/nYSHbwGjFqZi7QnY8A/giphy.gif'        // Fox in nature
 ];
 
 export default function LoadingAnimal() {
-  const [gifIndex, setGifIndex] = useState(() => Math.floor(Math.random() * gifs.length));
+  const [gifIndex, setGifIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    // Pick a new random gif each time loading starts
-    setGifIndex(Math.floor(Math.random() * gifs.length));
+    const randomIndex = Math.floor(Math.random() * gifs.length);
+    setGifIndex(randomIndex);
   }, []);
+
+  if (gifIndex === null) return null;
 
   return (
     <div className="flex flex-col items-center justify-center mt-6">
@@ -30,3 +33,4 @@ export default function LoadingAnimal() {
     </div>
   );
 }
+
